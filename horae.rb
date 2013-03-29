e4377b4050ce31e44fc26464b059dc0e87b0c0e7
Alex Thaler <alexgthaler@gmail.com>
2013-03-28 22:50:37 -0500

    added live result model and working on parsing logic

--- horae.rb
+++ horae.rb
@@ -1,4 +1,6 @@
 require 'watir-webdriver'
+require 'model/live_result'
+
 b = Watir::Browser.new
 b.goto 'http://metrarail.com/metra/wap/en/home/RailTimeTracker.html'
 line_select = b.select_list(:id => 'findLine-0')
@@ -7,30 +9,58 @@ schedule_end = b.select_list(:id => 'schedule-end')
 submit_button = b.img(:xpath => '//*[(@id = "schedule-group")]//img')
 edit_stations_button = b.a(:xpath => '//*[(@id = "schedule-group")]//*[contains(concat( " ", @class, " " ), concat( " ", "right", " " ))]')
 
-line_select.options.each do |option|
+# line_select.options.each do |option|
 
-    if !option.text.include? "Select"
-        puts option.text
-        line_select.select(option.text)
+    # if !option.text.include? "Select"
+        # puts option.text
+        
 
-        sleep(1)
+        # schedule_start.options.each do |option|
+        #     if !option.text.include? "Select" then
+        #         departing_station = option.text
+        #         schedule_start.select(option.text)
 
-        schedule_start.options.each do |option|
-            if !option.text.include? "Select" then
-                departing_station = option.text
-                schedule_start.select(option.text)
+        #         schedule_end.options.each do |option|
 
-                schedule_end.options.each do |option|
+        #             if !option.text.include? "Select" and !option.text.include? departing_station then
+        #                 schedule_end.select(option.text)
+        #                 submit_button.click
+        #                 sleep(1)
+        #                  edit_stations_button.click
+        #             end
+        #         end 
+        #     end
+        # end    
+    # end
+# end
 
-                    if !option.text.include? "Select" and !option.text.include? departing_station then
-                        schedule_end.select(option.text)
-                        submit_button.click
-                        sleep(1)
-                         edit_stations_button.click
-                    end
-                end 
-            end
-        end    
+line_select.select("Union Pacific / North Line")
+sleep(1)
+
+schedule_start.select("Ravenswood")
+schedule_end.select("Clybourn")
+
+submit_button.click
+sleep(1)
+
+result_rows = ['one', 'two', 'three']
+result_columns = [0, 1, 2]
+
+result_rows.each do |row|
+    if b.tr(:class => row).exists? then
+        table_row = b.tr(:class => row)
+
+        train_num = table_row.td(:index => result_columns[0]).text unless !table_row.td(:index => result_columns[0]).exists?
+        scheduled_time = table_row.td(:index => result_columns[1]).text unless !table_row.td(:index => result_columns[1]).exists?
+        actual_time = table_row.td(:index => result_columns[2]).text unless !table_row.td(:index => result_columns[2]).exists?
+
+        result = 
+        # result_columns.each do |column|
+        #     if b.tr(:class => row).td(:index => column).exists? then
+        #         puts b.tr(:class => row).td(:index => column).text
+        #     end
+        # end
     end
 end
+
 b.close
---
+++ src/model/live_result.rb
@@ -0,0 +1,9 @@
+class LiveResult 
+    attr_accessor :train_num, :scheduled_time, :actual_time
+
+    def initialize(train_num, scheduled_time, actual_time)
+        @train_num = train_num
+        @scheduled_time = scheduled_time
+        @actual_time = actual_time
+    end
+end
