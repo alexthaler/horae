1251aee65012e3512afe8a7494047dc637fc4388
Alex Thaler <alexgthaler@gmail.com>
2013-04-08 19:50:11 -0500

    changed directory structure to be more rubyish

--- lib/gtfs_parser.rb
+++ lib/gtfs_parser.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 module Horae
     class GtfsParser
 
---
+++ spec/gtfs_parser_spec.rb
@@ -0,0 +1,19 @@
+require 'gtfs_parser'
+
+describe 'GTFSParser' do
+
+  it "returns the number of stops in the file minus one" do
+    parser = Horae::GtfsParser.new
+    parsed_stops = parser.parse_file('spec_data/test_stops.txt')
+    parsed_stops.length.should eql 2
+  end
+
+  it "returns correctly parsed stop ids" do
+    parser = Horae::GtfsParser.new
+    parsed_stops = parser.parse_file('spec_data/test_stops.txt')
+
+    parsed_stops[0]['stop_id'].should eql 'GENEVA'
+    parsed_stops[1]['stop_id'].should eql 'WCHICAGO'
+  end
+
+end
