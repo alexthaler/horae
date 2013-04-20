5c2c9756a4ff99b2918f9f593666533af43e1d60
Alex Thaler <alexgthaler@gmail.com>
2013-04-20 15:55:10 -0500

    added stops and status page

--- public/js/horae.js
+++ public/js/horae.js
@@ -4,8 +4,19 @@ horae.config(function($routeProvider) {
 	$routeProvider
 		.when('/', {controller:'RouteCtrl', templateUrl:'routes.html'})
 		.when('/route/:route_id', {controller:'StopCtrl', templateUrl:'stops.html'})
+		.when('/route/:route_id/:origin_id', {controller:'StopCtrl', templateUrl:'stops.html'})
+		.when('/status/:route_id/:origin_id/:dest_id', {controller:'StatusCtrl', templateUrl:'status.html'})
 		.otherwise({redirectTo:'/'});
 });
+
+horae.controller('StatusCtrl', ['$scope', '$http', '$routeParams', function StatusCtrl($scope, $http, $routeParams) {
+	$http.get("/live/" + $routeParams.route_id + "/" + $routeParams.origin_id + "/" + $routeParams.dest_id)
+	.success(function(data) {
+		$scope.statuses = data
+	}).error(function() {
+		//nothing
+	});
+}]);
 
 horae.controller('RouteCtrl', ['$scope', '$http', function RouteCtrl($scope, $http) {
 	$http.get("/routes")
@@ -30,4 +41,12 @@ horae.controller('StopCtrl', ['$scope', '$http', '$routeParams', function StopCt
 	}).error(function() {
 		//skip
 	});
+
+	$scope.generateLink = function(stop) {
+		if ($routeParams.origin_id == undefined) {
+			return "#/route/" + $routeParams.route_id + "/" + stop.stop_id;
+		} else {
+			return "#/status/" + $routeParams.route_id + "/" + $routeParams.origin_id + "/" + stop.stop_id;
+		}
+	}
 }]);
